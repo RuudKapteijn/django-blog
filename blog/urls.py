fda20ee2ee8900588b3be53deb94b9b31ee586ac
RuudKapteijn <ruud.kapteijn@gmail.com>
2019-02-09 18:58:58 +0100

    Additional list view.

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('list/', views.post_sel_list, name='post_sel_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
 
